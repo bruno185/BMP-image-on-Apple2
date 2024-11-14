601634ab30cb0ce84cb35bc4761a52bc3fe03f48
bruno185 <salut.lespotes@gmail.com>
2024-11-14 21:39:40 +0100

    OK clear

--- asmdemo.s
+++ asmdemo.s
@@ -8,8 +8,8 @@
         * BMP image must have 1 bit per pixel
         * Dimension must not exceed 280 x 192
         * Every bit (= pixel) in BMP file is doubled horizontally 
-* to respect image aspect ratio (more or less)
-* The image is drawn line by line, using PaintBits function of Graphics Primitives package
+* to respect image aspect ratio (more or less),
+* the image is drawn line by line, using PaintBits function of Graphics Primitives package
 *
 *
 * Memory :
